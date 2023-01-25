2cdb827de293b69bab2e11e8080599f2f9bf727d
demuu <preasim.kr@gmail.com>
2023-01-25 13:27:18 +0900

    coding test

--- src/algorithm/VaultSteal.java
+++ src/algorithm/VaultSteal.java
@@ -5,7 +5,7 @@ class VaultSteal {
         int target = 30;
         int[] type = {5,6,7};
 
-        long[] dp = new long[target+1];
+        long[] dp = new long[target+1]; // int 가 아닌 long
         dp[0] = 1;
 
         for(int coin : type){
@@ -14,6 +14,6 @@ class VaultSteal {
                 dp[i] += dp[i-coin];
             }
         }
-        System.out.println("result : "+dp[target]);
+        System.out.println("result : " + dp[target]);
     }
 }
--- src/daily/InsertDash.java
+++ src/daily/InsertDash.java
@@ -9,14 +9,20 @@ class InsertDash {
         // 2. index i번째 값과 i+1값이 홀수일 경우
         // 3. 그 사이에 "-" 추가
         String str = "454793";
-        ArrayList<String> list = new ArrayList<>(Arrays.asList(str.split("")));
-        for(int i=0; i<str.length();i++){
-            int num = Integer.parseInt(list.get(i));
-            int num2 = Integer.parseInt(list.get(i+1));
-            if(num%2==1 && num2%2==1){
-                list.add(i-1,"-");
+
+//        if(str.length() == 0) {
+//            return null;
+//        }
+        String result = "" + str.charAt(0);
+
+        for(int i = 1; i < str.length(); i++) {
+            int preNumber = Character.getNumericValue(str.charAt(i - 1)) % 2;
+            int curNumber = Character.getNumericValue(str.charAt(i)) % 2;
+            if(preNumber != 0 && curNumber != 0) {
+                result = result + "-";
             }
+            result = result + str.charAt(i);
         }
-        System.out.println(list);
+        System.out.println(result);
     }
 }
