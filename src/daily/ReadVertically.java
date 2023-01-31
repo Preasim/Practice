04332a12c5432a352743657d56d93bccc13999ba
demuu <preasim.kr@gmail.com>
2023-02-01 00:44:35 +0900

    Java daily 13

--- src/daily/ReadVertically.java
+++ src/daily/ReadVertically.java
@@ -4,21 +4,33 @@ import java.util.Arrays;
 
 public class ReadVertically {
     public static void main(String[] args) {
-        String[] arrs = {
-                "hii",
+        String[] arr = {
+                "hi",
                 "wolrd"
         };
-        String test = arrs[0];
-        String test2 = arrs[1];
-        String arr = "";
 
+        String test = arr[0];
+        String test2 = arr[1];
+        String result = "";
+        int count = 0;
 
-        for(int i=0; i<test.length(); i++){
-                arr += test.charAt(i);
-                arr += test2.charAt(i);
+        if(test.length() < test2.length()){
+            count = test.length();
+        } else {
+            count = test2.length();
+        }
+
+        for(int i=0; i<count; i++){
+                result += test.charAt(i);
+                result += test2.charAt(i);
 
         }
-        System.out.println(arr);
+        if(test.substring(count, test.length()).length() != 0){
+            result += test.substring(count, test.length());
+        } else {
+            result += test2.substring(count, test2.length());
+        }
+        System.out.println(result);
 
     }
 }
