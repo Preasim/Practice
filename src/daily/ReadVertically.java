17a7083fe18e061ccf4e1614db62737c400ff50f
demuu <preasim.kr@gmail.com>
2023-01-31 23:43:05 +0900

    Java daily 13

--- src/daily/ReadVertically.java
+++ src/daily/ReadVertically.java
@@ -1,46 +1,24 @@
 package daily;
 
-import java.io.IOException;
 import java.util.Arrays;
 
 public class ReadVertically {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String[] arrs = {
-                "hello",
-                "wolrd",
+                "hii",
+                "wolrd"
         };
-        String str = new String(Arrays.toString(arrs));
-        System.out.println(str);
+        String test = arrs[0];
+        String test2 = arrs[1];
+        String arr = "";
 
 
-//        for(int i=0; i<arrs.length; i++){
-//            String[] test = new String[]{arrs[i]};
-//            System.out.println(Arrays.toString(test));
-//        }
+        for(int i=0; i<test.length(); i++){
+                arr += test.charAt(i);
+                arr += test2.charAt(i);
 
+        }
+        System.out.println(arr);
 
-
-
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringBuilder sb = new StringBuilder();
-//        char[][] arr = new char[5][15];
-//        for (int i = 0; i < 5; i++) {
-//            String temp = br.readLine();
-//            for (int j = 0; j < temp.length(); j++) {
-//                arr[i][j]=temp.charAt(j);
-//            }
-//            for (int j = temp.length(); j < 15; j++) {
-//                arr[i][j]=' ';
-//            }
-//        }
-//
-//        for (int i = 0; i < 15; i++) {
-//            for (int j = 0; j < 5; j++) {
-//                if(arr[j][i]!=' ') {
-//                    sb.append(arr[j][i]);
-//                }
-//            }
-//        }
-//        System.out.println(sb);
     }
 }
