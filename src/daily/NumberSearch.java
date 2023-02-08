4845045b27fb92ff2d9a77ba18b7b1bb79a02c32
demuu <preasim.kr@gmail.com>
2023-02-08 10:49:39 +0900

    Java daily 18

--- src/daily/NumberSearch.java
+++ src/daily/NumberSearch.java
@@ -1,7 +1,24 @@
 package daily;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class NumberSearch {
     public static void main(String[] args) {
+        String str = "Hello6 9World 2,";
 
+        String Wstr = str.replaceAll("[0-9\\s]", "");
+        int intStr = Integer.parseInt(str.replaceAll("[^0-9]",""));
+        int[] intStrArr = Stream.of(String.valueOf(intStr).split(""))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+
+        int num = 0;
+        for(int i=0; i<intStrArr.length; i++){
+            num+=intStrArr[i];
+        }
+        num = num / Wstr.length();
+        System.out.println(num);
     }
 }
