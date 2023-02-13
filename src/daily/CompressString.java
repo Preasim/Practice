c3e3b69ad75061528d913adb50c0b617a1376775
demuu <preasim.kr@gmail.com>
2023-02-13 11:46:05 +0900

    Java daily 20

---
+++ src/daily/CompressString.java
@@ -0,0 +1,32 @@
+package daily;
+
+import java.util.Objects;
+
+public class CompressString {
+    public static void main(String[] args) {
+        String str = "wwwggoppopppp";
+
+        char before = str.charAt(0);
+
+        int count = 1;
+        String result = "";
+
+        str = str + ' ';
+        for(int i=1; i<str.length(); i++){
+            if(before == str.charAt(i)){
+                count++;
+            } else {
+                if(count>=3){
+                    result = result + count + before;
+                } else {
+                    for (int j=0; j<count; j++){
+                        result = result + before;
+                    }
+                }
+                before = str.charAt(i);
+                count = 1;
+            }
+        }
+        System.out.println(result);
+    }
+}
