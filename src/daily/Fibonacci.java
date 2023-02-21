4af4567150d8a44eaefcf7752abd2c89d5508988
demuu <preasim.kr@gmail.com>
2023-02-21 10:15:13 +0900

    Java daily 23

--- src/daily/Fibonacci.java
+++ src/daily/Fibonacci.java
@@ -1,38 +1,52 @@
 package daily;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 class Fibonacci {
     public static void main(String[] args) {
         FibonacciSolution fibonacciSolution = new FibonacciSolution();
 
-        long output = fibonacciSolution.fibonacci(30);
+        long output = fibonacciSolution.fibonacci(10);
         System.out.println("output : " + output);
     }
 }
 
 class FibonacciSolution {
-    public long fibonacci(int num){
+    public int fibonacci(int num){
+//        1번 풀이 ----------------
 //        if(num==0) return 0;
 //        if(num==1||num==2) return 1;
 //        return fibonacci(num-1) + fibonacci(num-2);
 
-        if(num<2) {
-            return num;
-        } else {
-            long[] cache = new long[num+1];
-            Arrays.fill(cache, -1);
-            cache[0] = 0;
-            cache[1] = 1;
 
-            if(cache[num] != -1){
-                return cache[num];
-            } else {
-                cache[num] = fibonacci(num-1) + fibonacci(num-2);
-                return cache[num];
-            }
-        }
+        // 2번 풀이 -----------------------
+//        if(num<2) {
+//            return num;
+//        } else {
+//            long[] cache = new long[num+1];
+//            Arrays.fill(cache, -1);
+//            cache[0] = 0;
+//            cache[1] = 1;
+//
+//            if(cache[num] != -1){
+//                return (int) cache[num];
+//            } else {
+//                cache[num] = fibonacci(num-1) + fibonacci(num-2);
+//                return (int) cache[num];
+//            }
+//        }
 
-    // ㅈㄴ 개빡셈;;
+        // 3번 풀이 -----------------------
+
+        ArrayList<Integer> memo = new ArrayList<>();
+        memo.add(0);
+        memo.add(1);
+
+        return (int) aux(memo, num);
+    }
+    public long aux(ArrayList<Integer> memo, int num){
+        if(memo.size() <= num) memo.add((int) (aux(memo, num-1) + aux(memo, num-2)));
+        return memo.get(num);
     }
 }
