92f0dcd2720b22119dded5b7cce92883a27f90b0
demuu <preasim.kr@gmail.com>
2023-02-24 00:44:38 +0900

    Java daily 24

--- src/daily/IsSubsetOf.java
+++ src/daily/IsSubsetOf.java
@@ -1,6 +1,9 @@
 package daily;
 
+
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class IsSubsetOf {
     public static void main(String[] args) {
@@ -8,12 +11,36 @@ class IsSubsetOf {
 
         boolean output = isSubsetOfSolution.isSubsetOf(
                 new int[]{1,2,3,4,5},
-                new int[]{1,3});
+                new int[]{1,3,16});
+        System.out.println(output);
     }
 }
 
 class IsSubsetOfSolution {
     public boolean isSubsetOf(int[] base, int[] sample){
+        // 풀이 1---------------------------------
+//        int count = 0;
+//        if(sample.length == 0) return false;
+//
+//        for(int i=0; i<sample.length; i++){
+//            for(int j=0; j<base.length; j++){
+//                if(sample[i] == base[j]) count++;
+//            }
+//        }
+//        if(count == sample.length) return true;
+//        return false;
 
+        // 풀이 2 -------------------
+        List<Integer> intList
+                = Arrays.stream(base)
+                .boxed()
+                .collect(Collectors.toList());
+
+        for (int i:sample){
+            if(!intList.contains(i)){
+                return false;
+            }
+        }
+        return true;
     }
 }
