1b412ef2fcbec7161e585b9e8ad6dcef7db95040
demuu <preasim.kr@gmail.com>
2023-03-03 11:10:05 +0900

    Java daily 26

---
+++ src/daily/TreeDFS.java
@@ -0,0 +1,38 @@
+package daily;
+
+import java.util.ArrayList;
+
+public class TreeDFS {
+    public static void main(String[] args) {
+
+    }
+}
+
+public class TreeDFSSolution {
+    public ArrayList<String> dfs(tree node){
+        // TODO :
+    }
+    public static class tree {
+        private String value;
+        private ArrayList<tree> children;
+
+        public tree(String data) {
+            this.value = data;
+            this.children = null;
+        }
+
+        public tree addChildNode(tree node) {
+            if(children == null) children = new ArrayList<>();
+            children.add(node);
+            return children.get(children.size() - 1);
+        }
+
+        public String getValue() {      //현재 노드의 데이터를 반환
+            return value;
+        }
+
+        public ArrayList<tree> getChildrenNode() {
+            return children;
+        }
+    }
+}
